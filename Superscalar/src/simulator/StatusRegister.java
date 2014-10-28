f7f0ac93d226141d7a6aa843fde045ff9ffce59b
Maciej Kumorek <mkumorek@gmail.com>
2014-10-28 11:30:02 +0000

    Added status register

--- Superscalar/src/simulator/StatusRegister.java
+++ Superscalar/src/simulator/StatusRegister.java
@@ -3,5 +3,31 @@ package simulator;
 /**
  * Created by Maciej Kumorek on 10/28/2014.
  */
-public class StatusRegister {
+public class StatusRegister extends Register {
+
+    public enum Status {
+        EQ(0),
+        LT(1),
+        LE(2),
+        GT(3),
+        GE(4);
+
+        private final int value;
+
+        private Status(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+    }
+
+    public Status getStatus() {
+        return Status.values()[this.getValue()];
+    }
+
+    public void setStatus(Status status) {
+        this.setValue(status.getValue());
+    }
 }
