71fadf407652eccd835c363dc6ffcdb85ded7f7b
Maciej Kumorek <mkumorek@gmail.com>
2014-11-22 14:17:03 +0000

    New folder structure

---
+++ simulator/src/main/java/org/mk0934/simulator/App.java
@@ -0,0 +1,13 @@
+package org.mk0934.simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
