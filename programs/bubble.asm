66892963f640ee7fc2b3cf11ba750a686d180ae8
Maciej Kumorek <mkumorek@gmail.com>
2014-11-24 16:09:37 +0000

    Improved bubble sort

--- programs/bubble.asm
+++ programs/bubble.asm
@@ -2,28 +2,26 @@
 start:
     MOV r0, 0x1
 repeat:
+    MOV r2, 0x4         ; i = 1
     MOV r1, 0x0         ; swapped  = false
-    MOV r2, 0x1         ; i = 1
 forloop:
-    CMP r3, r2, 0x9
+    CMP r3, r2, 0x24
     BGE r3, endfor      ; end for loop if i > len(array)
-    SUB r4, r2, 0x1     ; i-1
-    MUL r5, r2, 0x4     ; offset for array[i]
-    MUL r6, r4, 0x4     ; offset for arrray[i-1]
-    LDM r8, r5, array   ; load array[i]
-    LDM r9, r6, array   ; load array[i-i]
+    SUB r4, r2, 0x4     ; i-1 
+    LDM r8, r2, array   ; a =  array[i]
+    LDM r9, r4, array   ; b = array[i-i]
     CMP r7, r8, r9
     BGE r7, next
 swap:
-    STM r9, r5, array
-    STM r8, r6, array
-    MOV r1, 0x1
+    STM r9, r2, array       ; array[i] = b;
+    STM r8, r4, array       ; array[i-i] = a; 
+    MOV r1, 0x1             ; swapped = true
 next:
-    ADD r2, r2, 0x1
-    JMP forloop
+    ADD r2, r2, 0x4         ; i++
+    JMP forloop             ; continue;
 endfor:
-    CMP r3, r1, 0x1         ; if(swap == true)
-    BEQ r3, repeat          ;   goto repeat
+    CMP r3, r1, 0x1         ; if(swapped == true)
+    BEQ r3, repeat          ; then goto repeat
 end:
     SVC 0
 array:
